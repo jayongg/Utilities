7710a6dba658fe91565620b66428c96eb46d750e
jayongg <jayongg_2001@hotmail.com>
2016-08-21 21:43:14 -0700

    add GameBarLoader.csproj

---
+++ GameBarLoader/GameBarLoader/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using WindowsInput;
+using WindowsInput.Native;
+
+namespace GameBarLoader
+{
+    static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            var sim = new InputSimulator();
+
+            var args = Environment.GetCommandLineArgs();
+
+            if (args.Length < 2)
+            {
+                sim.Keyboard
+                   .ModifiedKeyStroke(VirtualKeyCode.LWIN, VirtualKeyCode.VK_G);
+                return;
+            }
+
+            switch (args[1].ToUpperInvariant())
+            {
+                case "S":
+                    sim.Keyboard
+                        .Sleep(1000)
+                        .ModifiedKeyStroke(new VirtualKeyCode[] { VirtualKeyCode.LWIN, VirtualKeyCode.MENU }, VirtualKeyCode.SNAPSHOT);
+                    break;
+                case "R":
+                    sim.Keyboard
+                        .Sleep(1000)
+                        .ModifiedKeyStroke(new VirtualKeyCode[] { VirtualKeyCode.LWIN, VirtualKeyCode.MENU }, VirtualKeyCode.VK_R);
+                    break;
+                case "G":
+                    sim.Keyboard
+                        .Sleep(1000)
+                        .ModifiedKeyStroke(new VirtualKeyCode[] { VirtualKeyCode.LWIN, VirtualKeyCode.MENU }, VirtualKeyCode.VK_G);
+                    break;
+            }
+        }
+    }
+}
